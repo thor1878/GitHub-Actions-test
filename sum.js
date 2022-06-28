8e0bac6e8e66b6eb1c83ed4dd950bd18a001fa9b
Thorbjørn Møller <66469783+thor1878@users.noreply.github.com>
2022-06-29 01:59:29 +0200

    Update sum.js

--- sum.js
+++ sum.js
@@ -2,8 +2,12 @@ function sum(a, b) {
   return a + b;
 }
 
-const sum3 function(a, b, c)  {
+const sum3 = async function(a, b, c)  {
   return a + b + c;
+}
+
+function() {
+  return;
 }
 
 module.exports = { sum, sum3 };
